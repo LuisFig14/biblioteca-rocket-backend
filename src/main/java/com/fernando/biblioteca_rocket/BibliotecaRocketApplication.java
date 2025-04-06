53cfe3a4a9d0635926e90adca185f03c222c79de
Fernando Licea <fernandolic1407@gmail.com>
2025-04-05 23:47:40 -0600

    first commit test rocket code fernado licea

---
+++ src/main/java/com/fernando/biblioteca_rocket/BibliotecaRocketApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.biblioteca_rocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaRocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaRocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/biblioteca_rocket/BibliotecaRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.biblioteca_rocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaRocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
